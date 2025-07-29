5866ea7f7e9537c62e8a16bf130852859d323e95
smchardie1 <mchardie.stella@gmail.com>
2025-07-29 21:31:11 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,9 +1,11 @@
-//green dino
-let green_x = 80
-let green_y = 142
-let green_neck_height = 90 //90 default
-let green_head_y = green_y-green_neck_height+30
-let tri_x = 128
+//brachio dino
+let brachio_scale = 0.8; // default: 1 (100% size)
+let tri_scale = 1.2;     // you can change this as needed
+let brachio_x = 60
+let brachio_y = 150
+let brachio_neck_height = 150 //90 default
+let brachio_head_y = brachio_y-brachio_neck_height+30
+let tri_x = 120
 let tri_y = 48
 
 
@@ -26,12 +28,18 @@ function setup_wallpaper(pWallpaper) {
 function wallpaper_background() {
   background(255); 
 }
-
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  greenDino();
+  hstripes_dark();
+  hstripes_light();
+  brachioDino();
   triDino();
 }
-function greenDino (){
+function brachioDino() {
+  push();
+  translate(brachio_x, brachio_y);   // Move origin to dino position
+  scale(brachio_scale, brachio_scale); // Add - to invert it
+  translate(-brachio_x, -brachio_y); // Reset origin for drawing
+
   g_scales();
   g_body();
   g_head();
@@ -40,157 +48,198 @@ function greenDino (){
   g_stomach();
   g_front_feet();
   g_tail();
+
+  pop();
 }
 function g_body() {
   noStroke();
   fill(147, 196, 125);
-  rect(green_x-50, green_y, 20, -green_neck_height);
-  ellipse(green_x, green_y, 100, 50);
+  rect(brachio_x-50, brachio_y, 20, -brachio_neck_height);
+  ellipse(brachio_x, brachio_y, 100, 50);
 }
 function g_head() {
   fill(112, 159, 91);
-  ellipse(green_x-31, green_head_y-36, 10);
-  ellipse(green_x-47, green_head_y-45, 10);
+  ellipse(brachio_x-31, brachio_head_y-36, 10);
+  ellipse(brachio_x-47, brachio_head_y-45, 10);
   fill(147, 196, 125);
-  ellipse(green_x-50, green_head_y-30, 40, 30);
+  ellipse(brachio_x-50, brachio_head_y-30, 40, 30);
   fill(84, 120, 67);
-  ellipse(green_x-62, green_head_y-27, 3);
+  ellipse(brachio_x-62, brachio_head_y-27, 3);
 }
 function g_eyes() {
   fill(0);
-  ellipse(green_x-48, green_head_y-33, 5);
+  ellipse(brachio_x-48, brachio_head_y-33, 5);
 }
 function g_stomach() {
   fill(210, 244, 195);
-  ellipse(green_x, green_y+12.5, 85, 25);
+  ellipse(brachio_x, brachio_y+12.5, 85, 25);
 }
 function g_back_feet() {
   fill(84, 120, 67);
-  rect(green_x-40, green_y+10, 20, 25);
-  rect(green_x+10, green_y+10, 20, 25);
+  rect(brachio_x-40, brachio_y+10, 20, 25);
+  rect(brachio_x+10, brachio_y+10, 20, 25);
 }
 function g_front_feet() {
   fill(112, 159, 91);
-  rect(green_x-35, green_y+5, 20, 35);
-  arc(green_x-25, green_y+6, 20, 25, 180, 360, OPEN);
-  rect(green_x+15, green_y+5, 20, 35);
-  arc(green_x+25, green_y+6, 20, 25, 180, 360, OPEN);
+  rect(brachio_x-35, brachio_y+5, 20, 35);
+  arc(brachio_x-25, brachio_y+6, 20, 25, 180, 360, OPEN);
+  rect(brachio_x+15, brachio_y+5, 20, 35);
+  arc(brachio_x+25, brachio_y+6, 20, 25, 180, 360, OPEN);
 }
 function g_tail() {
   fill(147, 196, 125);
   beginShape();
-  curveVertex(green_x+40, green_y-15);
-  curveVertex(green_x+75, green_y+10);
-  curveVertex(green_x+75, green_y+35);
-  curveVertex(green_x+50, green_y+55);
-  curveVertex(green_x+60, green_y+22.5);
-  curveVertex(green_x+45, green_y+7);
-  curveVertex(green_x+40, green_y-15);
-  curveVertex(green_x+75, green_y+10);
-  curveVertex(green_x+74, green_y+35);
+  curveVertex(brachio_x+40, brachio_y-15);
+  curveVertex(brachio_x+75, brachio_y+10);
+  curveVertex(brachio_x+75, brachio_y+35);
+  curveVertex(brachio_x+50, brachio_y+55);
+  curveVertex(brachio_x+60, brachio_y+22.5);
+  curveVertex(brachio_x+45, brachio_y+7);
+  curveVertex(brachio_x+40, brachio_y-15);
+  curveVertex(brachio_x+75, brachio_y+10);
+  curveVertex(brachio_x+74, brachio_y+35);
   endShape();
 }
 function g_scales () {
   fill(112, 159, 91);
   noStroke();
   //neck
-  if (green_neck_height > 110) {
-    ellipse(green_x-30, green_y-115, 10);
+  if (brachio_neck_height > 210) {
+    ellipse(brachio_x-30, brachio_y-215, 10);
   }
-  if (green_neck_height > 90) {
-    ellipse(green_x-30, green_y-95, 10);
+  if (brachio_neck_height > 190) {
+    ellipse(brachio_x-30, brachio_y-195, 10);
   }
-  if (green_neck_height > 70) {
-    ellipse(green_x-30, green_y-75, 10);
+  if (brachio_neck_height > 170) {
+    ellipse(brachio_x-30, brachio_y-175, 10);
   }
-  if (green_neck_height > 50) {
-    ellipse(green_x-30, green_y-55, 10);
+  if (brachio_neck_height > 150) {
+    ellipse(brachio_x-30, brachio_y-155, 10);
   }
-  if (green_neck_height > 30) {
-    ellipse(green_x-30, green_y-35, 10);
+  if (brachio_neck_height > 130) {
+    ellipse(brachio_x-30, brachio_y-135, 10);
+  }
+  if (brachio_neck_height > 110) {
+    ellipse(brachio_x-30, brachio_y-115, 10);
+  }
+  if (brachio_neck_height > 90) {
+    ellipse(brachio_x-30, brachio_y-95, 10);
+  }
+  if (brachio_neck_height > 70) {
+    ellipse(brachio_x-30, brachio_y-75, 10);
+  }
+  if (brachio_neck_height > 50) {
+    ellipse(brachio_x-30, brachio_y-55, 10);
+  }
+  if (brachio_neck_height > 30) {
+    ellipse(brachio_x-30, brachio_y-35, 10);
   }
   //body
-  ellipse(green_x-20, green_y-22, 10);
-  ellipse(green_x, green_y-25, 10);
-  ellipse(green_x+20, green_y-22, 10);
-  ellipse(green_x+40, green_y-15, 10);
+  ellipse(brachio_x-20, brachio_y-22, 10);
+  ellipse(brachio_x, brachio_y-25, 10);
+  ellipse(brachio_x+20, brachio_y-22, 10);
+  ellipse(brachio_x+40, brachio_y-15, 10);
   //tail
-  ellipse(green_x+59, green_y-4, 10);
-  ellipse(green_x+75, green_y+10, 10);
-  ellipse(green_x+76, green_y+33, 10);
-  ellipse(green_x+60, green_y+50, 10);
+  ellipse(brachio_x+59, brachio_y-4, 10);
+  ellipse(brachio_x+75, brachio_y+10, 10);
+  ellipse(brachio_x+76, brachio_y+33, 10);
+  ellipse(brachio_x+60, brachio_y+50, 10);
 }
 function triDino() {
+  push();
+  translate(tri_x, tri_y);
+  scale(tri_scale);
+  translate(-tri_x, -tri_y);
+
   tri_back_legs();
   tri_body();
   tri_head();
   tri_eye();
   tri_stomach();
   tri_front_legs();
-  
+
+  pop();
 }
 function tri_body() {
+  noStroke();
   fill(251, 197, 200);
   ellipse(tri_x, tri_y, 47);
   //arc(122.5, 43, 95, 60, 358, 130);
-  arc(115, 33, 110, 80, 358, 75, OPEN);
-  stroke(0);
-  noStroke();
+  arc(tri_x-13, tri_y-15, 110, 80, 358, 75, OPEN);
   beginShape();
-  curveVertex(107, 48);
-  curveVertex(98, 60);
-  curveVertex(90, 62);
-  curveVertex(100, 72);
-  curveVertex(127, 71);
-  curveVertex(107, 48);
-  curveVertex(98, 60);
-  curveVertex(90, 62);
+  curveVertex(tri_x-21, tri_y-15);
+  curveVertex(tri_x-30, tri_y+12);
+  curveVertex(tri_x-38, tri_y+14);
+  curveVertex(tri_x-28, tri_y+24);
+  curveVertex(tri_x-1, tri_y+23);
+  curveVertex(tri_x-21, tri_y);
+  curveVertex(tri_x-30, tri_y+12);
+  curveVertex(tri_x-38, tri_y+14);
   endShape();
-  triangle(105.5, 42, 102, 52, 108, 52);
+  triangle(tri_x-22.5, tri_y-6, tri_x-26, tri_y+4, tri_x-20, tri_y+4);
   
 }
 function tri_head( ){
   fill(213, 102, 111);
-  ellipse(144, 45, 9) //bumps
-  ellipse(144, 33, 9) //bumps
-  ellipse(144, 21, 9) //bumps
+  ellipse(tri_x+16, tri_y-3, 9) //bumps
+  ellipse(tri_x+16, tri_y-15, 9) //bumps
+  ellipse(tri_x+16, tri_y-27, 9) //bumps
   fill(251, 197, 200);
-  triangle(145, 15, 145, 55, 180, 35);
-  rect(160, 27.5, 25, 15)
+  triangle(tri_x+17, tri_y-33, tri_x+17, tri_y+7, tri_x+52, tri_y-13);
+  rect(tri_x+32, tri_y-20.5, 25, 15)
   fill(213, 102, 111);
-  rect(148, 28, 12, 4);
-  rect(148, 38, 12, 4);
-  triangle(180, 28, 189, 20, 185, 32)
-  triangle(162, 25, 169, 15, 168, 28)
+  rect(tri_x+20, tri_y-20, 12, 4);
+  rect(tri_x+20, tri_y-10, 12, 4);
+  triangle(tri_x+52, tri_y-20, tri_x+61, tri_y-28, tri_x+57, tri_y-16)
+  triangle(tri_x+34, tri_y-23, tri_x+41, tri_y-33, tri_x+40, tri_y-20)
   fill(231, 142, 150);
-  ellipse(180, 37, 2.5)
+  ellipse(tri_x+52, tri_y-11, 2.5)
 }
 function tri_eye() {
   fill(0);
-  ellipse(168, 33, 5)
+  ellipse(tri_x+40, tri_y-15, 5)
 }
 function tri_front_legs() {
   fill(231, 142, 150);
-  ellipse(116, 64, 13);
-  rect(109.5, 64, 13, 16);
-  ellipse(144, 62, 13);
-  rect(137.5, 62, 13, 16);
+  ellipse(tri_x-12, tri_y+16, 13);
+  rect(tri_x-18.5, tri_y+16, 13, 16);
+  ellipse(tri_x+16, tri_y+14, 13);
+  rect(tri_x+9.5, tri_y+14, 13, 16);
 }
 function tri_back_legs() {
+  noStroke();
   fill(213, 102, 111);
-  rect(115.5, 61, 13, 16);
-  rect(143.5, 59, 13, 16);
+  rect(tri_x-12.5, tri_y+13, 13, 16);
+  rect(tri_x+15.5, tri_y+11, 13, 16);
 }
 function tri_stomach() {
   fill(255, 226, 228);
   beginShape();
-  vertex(122, 73);
-  vertex(138, 70);
-  vertex(157, 60);
-  vertex(163, 52.5);
-  vertex(143, 62);
-  vertex(120, 66);
+  vertex(tri_x-6, tri_y+25);
+  vertex(tri_x+10, tri_y+22);
+  vertex(tri_x+29, tri_y+12);
+  vertex(tri_x+35, tri_y+4.5);
+  vertex(tri_x+15, tri_y+14);
+  vertex(tri_x-8, tri_y+18);
   endShape();
-
 }
+function hstripes_light(){
+  noStroke();
+  fill(222, 236, 255);
+  rect(0, 0, 200, 15);
+  rect(0, 40, 200, 15);
+  rect(0, 80, 200, 15);
+  rect(0, 120, 200, 15);
+  rect(0, 160, 200, 15);
+}
+function hstripes_dark(){
+  noStroke();
+  fill(164, 198, 245);
+  rect(0, 5, 200, 15);
+  rect(0, 45, 200, 15);
+  rect(0, 85, 200, 15);
+  rect(0, 125, 200, 15);
+  rect(0, 165, 200, 15);
+}
+
 
